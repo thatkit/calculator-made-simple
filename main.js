638e31ed6ee48329b7232e5eb356d09d70588a29
ThatKit <emper137137@gmail.com>
2021-04-13 08:35:22 +1000

    create calculation string function

---
+++ main.js
@@ -0,0 +1,24 @@
+let calcString = '';
+
+//Integers
+const num0 = 0;
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+const num4 = 4;
+const num5 = 5;
+const num6 = 6;
+const num7 = 7;
+const num8 = 8;
+const num9 = 9;
+//Operators and a dot
+const dot = '.';
+const pls = '+';
+const mns = '-';
+const mlt = '*';
+const dvd = '/';
+
+const createCalcString = button => {
+    calcString += button;
+    return calcString;
+}
